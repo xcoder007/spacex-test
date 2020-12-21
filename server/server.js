b00591e93694132d0ae521284f7fc7f78d5bb617
Aarvey Raj <aarveyraj@yahoo.co.in>
2020-12-21 11:17:14 +0530

    Files rearranged

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
-app.use(express.static(path.join(__dirname, "static")));
+console.log("Server- path", __dirname);
+console.log("Server- path -static ", path.join(__dirname, "..", "static"));
+app.use(express.static(path.join(__dirname, "..", "static")));
 
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "static", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "static", "index.html"));
 });
 
 const port = process.env.PORT || 8000;
